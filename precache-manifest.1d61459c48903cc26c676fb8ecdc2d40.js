f4e6341bfcc670f66818f8b9aa9b7ae310fbf119
Code Sandbox <hello@codesandbox.io>
2022-05-18 10:08:12 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1d61459c48903cc26c676fb8ecdc2d40.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8932381e85e922cba8103dc6ae9641",
+    "url": "https://sahildixit01.github.io/csb-ri8xy9/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "https://sahildixit01.github.io/csb-ri8xy9/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "75b9dfcc2645dc9f4135",
+    "url": "https://sahildixit01.github.io/csb-ri8xy9/static/js/main.913fde21.chunk.js"
+  },
+  {
+    "revision": "97bacefc0306b46d6802",
+    "url": "https://sahildixit01.github.io/csb-ri8xy9/static/js/runtime-main.34708f3b.js"
+  }
+]);
